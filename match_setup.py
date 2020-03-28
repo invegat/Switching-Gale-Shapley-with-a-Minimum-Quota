fa8114a60a446c0c8fb2849fe493c1f994b423bc
Mark Oliver <mark.c.oliver@gmail.com>
2020-03-27 20:11:07 -0400

    still debugging

--- match_setup.py
+++ match_setup.py
@@ -2,6 +2,8 @@ import random
 from match import Matcher
 from person import Person
 from flask import jsonify
+from collections import defaultdict
+import copy
 
 
 def setup(v_, j_):
@@ -37,6 +39,7 @@ def setup(v_, j_):
     for p in jobs:
         w__ = j_[p]
         J[Person(p, int(w__[-1]))] = w__[:-1]
+    print(f'initial J keys {J.keys()}')
 
     V = {}
     prefs = j_[list(j_.keys())[0]]
@@ -73,40 +76,62 @@ def setup(v_, j_):
 
     C = defaultdict(list)
     jKeys = []
+    loop = 0
     while len(J) >= len(V):
         match = Matcher(V, J, forbidden, forbidden_v)
 
         # match volunteers and jobs; returns a mapping of jobs to volunteers
         matches = match()
-        if C is None:
-            C = ((value, [key]) for key, value in enumerate(matches))
-        else:
-            for key, value in enumerate(matches):
-                C[value].append(key)
+        print(f'loop {loop} matches key {matches.keys()}')
+        loop += 1
+        if loop > 2:
+            break
+        # if len(C) == 0:
+        #     C = dict((value, [key]) for key, value in enumerate(matches))
+        #     print('Initial C.keys()', C.keys())
+        # else:
+        for _, key in enumerate(matches.items()):
+            C[key[1]].append(key[0])
+        print('Initial C.keys()', C.keys())
+        print('Initial C.values()', C.values())
         jKeys.extend(list(matches.keys()))
 
-        J = dict(filter(lamba j, v: j not in jKeys, enumerate(J))
+        print(f"len jKeys {len(jKeys)}  len(J) {len(J)} ")
+        J_ = copy.deepcopy(J)
+        J = {}
+        for key, value in enumerate(J_.items()):
+            print(f'J.items() key {key}  value {value}')
+            if value[0] not in jKeys:
+                J[value[0]] = value[1]
+        print(f'let filtered J {len(J)}')
+
+        # J = dict((key, value) in enumerate(J.items()))
+
+        # J = dict((key, value) in enumerate(J.items()) if key not in jKeys)
+        # J = dict(filter(lamba j, v: j not in jKeys, enumerate(J))
         assert match.is_stable(matches)           # should be a stable matching
         print('stable match')
     if len(J) > 0:
-        V_=sorted(C.items(), key=lambda kv: len(kv[1]))[:len(J)]
-        match=Matcher(V_, J, forbidden, forbidden_v)
+        print("len(J) > 0")
+        V_ = sorted(C.items(), key=lambda kv: len(kv[1]))[:len(J)]
+        match = Matcher(V_, J, forbidden, forbidden_v)
 
         # match volunteers and jobs; returns a mapping of jobs to volunteers
-        matches=match()
+        matches = match()
         for key, value in enumerate(matches):
             C[value].append(key)
 
-
-
     # print('jobs', jobs)
-    a=[(key.n, [j.n for j in C[key]) for key in list(C.keys())]
+    print('C.keys()', C.keys())
+    print([(key, value) for key, value in enumerate(C)])
+    a = [(key.n, [j.n for j in value]) for key, value in enumerate(C.items())]
+    # a = [(key.n, [j.n for j in C[key]]) for key in list(C.keys())]
 
     # a=[(matches[key].n, key.n) for key in list(matches.keys())]
     return jsonify(a)
 
     # swap the volunteers of two jobs, which should make the matching unstable
-    a, b=random.sample(jobs.keys(), 2)
-    jobs[b], jobs[a]=jobs[a], jobs[b]
+    # a, b = random.sample(jobs.keys(), 2)
+    # jobs[b], jobs[a] = jobs[a], jobs[b]
 
-    match.is_stable(jobs, verbose=True)
+    # match.is_stable(jobs, verbose=True)
